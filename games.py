44d067b6952fe5a307476b590c2959522c3fa478
jodikrwn <jodikurni4wan@gmail.com>
2024-07-19 15:17:01 +0700

    first commit

---
+++ games.py
@@ -0,0 +1,54 @@
+# membuat games kertas batu gunting kertas
+def main():
+    print('Pilih salah satu!')
+    print('1. Kertas')
+    print('2. Batu')
+    print('3. Gunting')
+
+    pilih = int(input('Masukan pilihan anda: '))
+    import random
+    comp = random.randint(1,3)
+
+    if pilih == 1:
+        if comp == 1:
+            print('Kamu memilih kertas')
+            print('Komputer memilih kertas')
+            print('Seri!')
+        elif comp == 2:
+            print('Kamu memilih kertas')
+            print('Komputer memilih batu')
+            print('Komputer menang!')
+        else:
+            print('Kamu memilih kertas')
+            print('Komputer memilih gunting')
+            print('Komputer menang!')
+    elif pilih == 2:
+        if comp == 1:
+            print('Kamu memilih batu')
+            print('Komputer memilih kertas')
+            print('Kamu menang!')
+        elif comp == 2:
+            print('Kamu memilih batu')
+            print('Komputer memilih batu')
+            print('Seri!')
+        else:
+            print('Kamu memilih batu')
+            print('Komputer memilih gunting')
+            print('Komputer menang!')
+    elif pilih == 3:
+        if comp == 1:
+            print('Kamu memilih gunting')
+            print('Komputer memilih kertas')
+            print('Komputer menang!')
+        elif comp == 2:
+            print('Kamu memilih gunting')
+            print('Komputer memilih batu')
+            print('Kamu menang!')
+        else:
+            print('Kamu memilih gunting')
+            print('Komputer memilih gunting')
+            print('Seri!')
+    else:
+        print('Pilihan tidak tersedia')
+
+main()
